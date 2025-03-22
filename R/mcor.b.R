d782228f96de3d9a450ef8f08d8db0a384d80594
yurasmol <smolyakov@rambler.ru>
2025-03-22 23:33:39 +0900

    First Commit

---
+++ R/mcor.b.R
@@ -0,0 +1,449 @@
+# This file is a generated template, your changes will not be overwritten
+
+mCORClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+  "mCORClass",
+  inherit = mCORBase,
+  private = list(
+
+     .init=function() {
+        #if (grepl("Russian", Sys.getlocale(), fixed=TRUE)) options(OutDec=",")
+        #Sys.setlocale('LC_ALL', 'russian')
+        matrix <- self$results$matrix
+        vars   <- self$options$vars
+        nVars  <- length(vars)
+        ciw    <- self$options$ciWidth
+        mtord <- FALSE
+
+	#grp    <- self$options$group
+	#if (!is.null(grp)) {
+        #  grps  <- jmvcore::select(self$data, grp)
+        #  groupLevels <- base::levels(grps[!is.na(grps[[grp]]), grp])
+	#} else {
+	#  groupLevels <- c("")
+	#}
+
+        #for (g in groupLevels) {
+          for (i in seq_along(vars)) {
+            var <- vars[[i]]
+            matrix$addColumn(name=paste0(var, '[r]'), title=var,
+                type='number', format='zto')
+            matrix$addColumn(name=paste0(var, '[cil]'), title=var,
+                type='number', format='zto', visible='(method=="pearson" && ci)')
+            matrix$addColumn(name=paste0(var, '[ciu]'), title=var,
+                type='number', format='zto', visible='(method=="pearson" && ci)')
+            matrix$addColumn(name=paste0(var, '[p]'), title=var,
+                type='number', format='zto,pvalue', visible='(pval)')
+            matrix$addColumn(name=paste0(var, '[n]'), title=var,
+                type='integer', visible='(n)')
+          }
+        #}
+
+        for (i in seq_along(vars)) {
+            var <- vars[[i]]
+            values <- list()
+
+            for (j in seq(i, nVars)) {
+                v <- vars[[j]]
+                values[[paste0(v, '[r]')]]   <- ''
+                values[[paste0(v, '[cil]')]] <- ''
+                values[[paste0(v, '[ciu]')]] <- ''
+                values[[paste0(v, '[p]')]]   <- ''
+                values[[paste0(v, '[n]')]]   <- ''
+            }
+
+            values[[paste0(var, '[r]')]]   <- '\u2014'
+            values[[paste0(var, '[cil]')]] <- '\u2014'
+            values[[paste0(var, '[ciu]')]] <- '\u2014'
+            values[[paste0(var, '[p]')]]   <- '\u2014'
+            values[[paste0(var, '[n]')]]   <- '\u2014'
+
+            values[['.stat[ciu]']] <- jmvcore::format(.('{ciWidth}% CI Upper'), ciWidth=ciw)
+            values[['.stat[cil]']] <- jmvcore::format(.('{ciWidth}% CI Lower'), ciWidth=ciw)
+            rw <- matrix$setRow(rowKey=var, values)
+  
+        }
+        hyp <- self$options$get('hyp')
+        if (hyp=='pos') {
+            matrix$setNote('hyp', .('H\u2090 is positive correlation'))
+            hyp <- 'greater'
+        } else if (hyp=='neg') {
+            matrix$setNote('hyp', .('H\u2090 is negative correlation'))
+            hyp <- 'less'
+        } else {
+            matrix$setNote('hyp', NULL)
+            hyp <- 'two.sided'
+        }
+        if (self$options$adjust!='none') {
+            matrix$setNote("adjust", jmvcore::format(.("Simultaneous multiple correlation comparisons using {n} method"), n=self$options$adjust))
+        }
+     },
+
+     .run = function() {
+        options(OutDec=",")
+        matrix <- self$results$matrix
+        vars   <- self$options$vars
+        nVars  <- length(vars)
+        hyp    <- self$options$hyp
+
+        corr   <- matrix(, nrow=nVars, ncol=nVars); corr[1, 1] <- 1
+        colnames(corr) <- vars; rownames(corr) <- vars
+        corp   <- matrix(, nrow=nVars, ncol=nVars); corp[1, 1] <- 0
+        colnames(corp) <- vars; rownames(corp) <- vars
+        if (hyp=='pos') hyp <- 'greater'
+        else if (hyp=='neg') hyp <- 'less'
+        else hyp <- 'two.sided'
+        dat <- self$data
+        self$results$text$setContent(" ")
+
+	# data subset
+	subs <- ""
+	if (!is.null(self$options$group)) {
+	   subs <- paste(self$options$group, " == \"", self$options$selgroup,"\"", sep="")
+           self$results$text$setContent(paste("<h2>", subs, "</h2>", sep=""))
+	   sub <- paste("`", self$options$group, "`==\"", self$options$selgroup,"\"", sep="")
+           dat <- subset(dat, eval(parse(text=sub)))
+        }
+
+        if (self$options$adjust!='none' && nVars>1) {
+            for (i in 1:nVars) {
+              rowVarName <- vars[[i]]
+              rowVar <- jmvcore::toNumeric(dat[[rowVarName]])
+              for (j in 1:nVars) {
+                colVarName <- vars[[j]]
+                colVar <- jmvcore::toNumeric(dat[[colVarName]])
+                if (is.factor(rowVar) || is.factor(colVar)) mtord <- TRUE
+                else mtord <- FALSE
+                result <- private$.test(rowVar, colVar, hyp, mtord)
+                corr[i, j] <- corr[j, i] <- result$r
+                corp[i, j] <- corr[j, i] <- result$p
+              }
+            }
+            #if (self$options$hclust) {
+            #  hc  <- hclust(dist(corr, method="euclidean"),
+            #                method=self$options$clustMet)
+            #  vars <- vars[hc$order]
+            #}
+            pp <- corp[lower.tri(corp, diag=FALSE)]
+            pp <- p.adjust(pp, self$options$adjust)
+            corp[lower.tri(corp, diag=FALSE)] <- pp
+
+            pp <- corp[upper.tri(corp, diag=FALSE)]
+            pp <- p.adjust(pp, self$options$adjust)
+            corp[upper.tri(corp, diag=FALSE)] <- pp
+            #self$results$pre$setContent(corp)
+        }
+
+        if (nVars>1) {
+            for (i in 2:nVars) {
+                rowVarName <- vars[[i]]
+                rowVar <- jmvcore::toNumeric(dat[[rowVarName]])
+                ciw <- self$options$ciWidth / 100
+                for (j in seq_len(i-1)) {
+                    values <- list()
+                    colVarName <- vars[[j]]
+                    colVar <- jmvcore::toNumeric(dat[[colVarName]])
+
+                    if (is.factor(rowVar) || is.factor(colVar)) mtord <- TRUE
+                    else mtord <- FALSE
+
+                    result <- private$.test(rowVar, colVar, hyp, mtord)
+
+                    corr[i, j] <- result$r; corr[j, i] <- result$r; corr[i, i] <- 1
+                    if (self$options$adjust=='none') {
+                      #self$results$pre$setContent(paste(result))
+                      corp[i, j] <- result$p;
+                      corp[j, i] <- result$p;
+                      corp[i, i] <- 0
+                    }
+
+                    values[[paste0(colVarName, '[r]')]]   <- result$r
+                    values[[paste0(colVarName, '[cil]')]] <- result$cil
+                    values[[paste0(colVarName, '[ciu]')]] <- result$ciu
+                    #values[[paste0(colVarName, '[p]')]]   <- result$p
+                    values[[paste0(colVarName, '[p]')]]   <- corp[i, j]
+                    n <- sum(!(is.na(colVar) | is.na(rowVar)))
+                    values[[paste0(colVarName, '[n]')]]   <- n
+
+                    matrix$setRow(rowNo=i, values)
+
+                    if (mtord)
+                        matrix$addFootnote(rowNo=i, paste0(colVarName, '[r]'), .('Pearson correlation cannot be calculated for non-numeric values'))
+               }
+            }
+            cor   <- NULL
+            cor$r <- corr
+            cor$p <- corp
+            cor$s <- subs
+            image <- self$results$treeplot
+            if (self$options$hclust && nVars>2) {
+              hc <- hclust(dist(cor$r, method="euclidean"), method=self$options$clustMet)
+              cor$hc <- hc
+              cor$r <- cor$r[hc$order, hc$order]
+              cor$p <- cor$p[hc$order, hc$order]
+              image$setState(cor)
+            } else {
+              image$setState(NULL)
+            }
+            image <- self$results$plot
+            image$setState(cor)
+         }
+       },
+
+      .test=function(var1, var2, hyp, mtord) {
+        options(OutDec=",")
+        results <- list()
+        suppressWarnings({
+            if (mtord) {
+                results$r <- NaN
+                results$rp <- NaN
+                results$rciu <- NaN
+                results$rcil <- NaN
+            } else {
+                ciw <- self$options$ciWidth / 100
+                res <- try(cor.test(as.numeric(var1), as.numeric(var2),
+                           alternative=hyp, method=self$options$method,
+                           conf.level=ciw, na.action=na.omit))
+                if (!base::inherits(res, 'try-error')) {
+                    results$r   <- res$estimate
+                    results$p   <- res$p.value
+                    results$cil <- res$conf.int[1]
+                    results$ciu <- res$conf.int[2]
+                }
+                else {
+                    results$r   <- NaN
+                    results$rp  <- NaN
+                    results$cil <- NaN
+                    results$ciu <- NaN
+                }
+            }
+
+        }) # suppressWarnings
+
+        return(results)
+      },
+
+      .plot=function(image, ggtheme, theme, ...) {
+	  vars  <- self$options$vars
+          nVars <- length(self$options$vars)
+          key   <- image$key
+          if (is.null(image$state) || nVars<2) return(FALSE)
+          c <- image$state
+
+          if (!self$options$hclust && self$options$plotOrder!="original") {
+            order <- corrplot::corrMatOrder(c$r, order=self$options$plotOrder)
+            c$r   <- c$r[order, order]
+            c$p   <- c$p[order, order]
+          }
+
+	  vars  <- colnames(c$r)
+
+	  col <- c("#053061", "#2166AC", "#4393C3",
+		"#92C5DE", "#D1E5F0", "#FDDBC7", "#F4A582",
+		"#D6604D", "#B2182B", "#67001F")
+	  cols <- hcl.colors(10, palette="Blue-Red 3")
+	  if (self$options$signif=="all") {
+		insig <- "pch"
+		sigl  <- 1.2
+	  } else {
+		insig <- "blank"
+		sigl  <- as.double(self$options$signif)
+	  }
+	  plotMetU <- self$options$plotMetU
+	  plotMetL <- self$options$plotMetL
+	  pm <- plotMetU
+	  if (pm=="empty") pm = "circle"
+	  sl <- sigl
+	  ULe <- plotMetU==plotMetL && plotMetU=="empty"
+	  if (plotMetU!=plotMetL || ULe) sl=as.double(0)
+
+	  ncex <- 1.8-0.055*nVars
+          p <- corrplot::corrplot(c$r, p.mat=c$p,
+		tl.cex=1, tl.col="black", diag=FALSE,
+		mar=c(0, 0, 1, 0), main=c$s,
+		col=col, number.cex=ncex,
+		cl.pos=self$options$clPos, cl.cex=1.1,
+		sig.level=sl,
+		insig=ifelse(plotMetU!=plotMetL || ULe, "blank", insig), pch="",
+		method=pm,
+                addshade=c("negative", "positive", "all")[3], shade.lwd=3,
+		type="full")
+
+	  if ((plotMetU!=plotMetL && plotMetU!="empty") && !ULe) {
+            corrplot::corrplot(c$r, p.mat=c$p, add=TRUE, cl.pos="n", diag=FALSE,
+		col=col, number.cex=ncex, tl.pos="n",
+		sig.level=sigl, insig=insig, pch="",
+		method=plotMetU,
+                addshade=c("negative", "positive", "all")[3], shade.lwd=3,
+		type="upper")
+          }
+
+	  if ((plotMetU!=plotMetL && plotMetL!="empty") && !ULe) {
+            corrplot::corrplot(c$r, p.mat=c$p, add=TRUE, cl.pos="n", diag=FALSE,
+		col=col, number.cex=ncex, tl.pos="n",
+		sig.level=sigl, insig=insig, pch="",
+		method=plotMetL,
+                addshade=c("negative", "positive", "all")[3], shade.lwd=3,
+		type="lower")
+          }
+
+          par(mar=c(0,0,1,0))
+	  lines(c(nVars+0.5, 0.5), rev(c(nVars+0.5, 0.5)), lwd=3, col="lightgrey")
+	  posClust <- list()
+          if (self$options$hclust && nVars>2) {
+            hc <- c$hc
+            ct <- cutree(hc, k=ifelse(self$options$numClust>nVars,nVars,self$options$numClust))
+            ct <- ct[hc$order]
+            ct <- c(ct[!duplicated(ct)], ct[length(ct)])
+            posClust <- pmatch(names(ct), vars, nomatch=nVars)
+            corrplot::corrRect(p, name=names(ct), lwd=2, add=TRUE)
+          } else if (self$options$clustMan>"" && nVars>2) {
+            vec <- as.numeric(strsplit(self$options$clustMan, ",")[[1]])
+            posClust <- c(1, vec, nVars)
+            corrplot::corrRect(p, posClust, lwd=2, add=TRUE)
+	  }
+
+          # clusters numbers
+          if (length(posClust)>0) {
+            coor <- vector()
+            io   <- 0
+            nCl  <- length(posClust)
+            posClust[nCl] <- posClust[nCl]+1
+            for (i in posClust[-1]) {
+              cw   <- i-io
+              coor <- append(coor, cw/2+io)
+              io   <- i-1
+            }
+            Num<- diag(1:(nCl-1))
+            dn <- lower.tri(Num, diag=FALSE)
+            r  <- (nCl):(nCl^2)
+            r  <- r[1:(length(r)/2)]
+            Num[dn] <- r; Num <- t(Num); Num[dn] <- r; Num <- t(Num)
+            k <- 1
+            for (i in coor) {
+              for (j in coor) {
+                text(j, nVars-i+1, Num[k], cex=1.4, font=4, col="black")
+                k <- k + 1
+                #lines(c(nVars+0.5, 0.5), rev(c(nVars+0.5, 0.5)), lwd=2, col="lightgreen")
+              }
+            }
+
+            splAt  <- function(x, pos) unname(split(x, findInterval(x, pos)))
+            vec    <- splAt(1:nVars, posClust)
+            cmb    <- combn(1:(length(vec)), 2)
+            #self$results$pre$setContent(cmb)
+
+            images <- self$results$get('rplots')
+            #images <- groups$get(key=i)$assump$resPlots
+            #images <- self$results$rplots
+
+            for (i in 1:ncol(cmb)) {
+              cm  <- cmb[,i]
+              crr <- list()
+	      if (length(vec[[cm[1]]])>length(vec[[cm[2]]])) {
+                m1 <- vec[[cm[2]]]; m2 <- vec[[cm[1]]]
+              } else {
+                m1 <- vec[[cm[1]]]; m2 <- vec[[cm[2]]]
+              }
+              
+              crr$r <- c$r[m1, m2, drop=FALSE]
+              crr$p <- c$p[m1, m2, drop=FALSE]
+	      crr$m <- c$s
+
+	      key <- paste(cm[1], cm[2], sep="_")
+              image <- images$addItem(key)
+              nm <- jmvcore::format(.("Matrix #{i}"), i=i+nCl-1)
+              image$setTitle(nm)
+              image$setVisible(visible=TRUE)
+              #image$setStatus(c('complete', 'error', 'inited', 'running')[1])
+              image$setState(crr)
+            }
+          }
+
+	  print(p)
+	  return(TRUE)
+      },
+
+      .rplot=function(image, ggtheme, theme, ...) {
+          if (is.null(image$state)) return(FALSE)
+          cor   <- image$state
+	  col <- c("#053061", "#2166AC", "#4393C3",
+		"#92C5DE", "#D1E5F0", "#FDDBC7", "#F4A582",
+		"#D6604D", "#B2182B", "#67001F")
+	  plotMetU <- self$options$plotMetU
+	  ULe  <- plotMetU=="empty"
+	  if (ULe) pm = "circle"
+	  else pm <- plotMetU
+	  if (self$options$signif=="all") {
+		insig <- "pch"
+		sigl  <- 1.2
+	  } else {
+		insig <- "blank"
+		sigl  <- as.double(self$options$signif)
+	  }
+          nrow <- nrow(cor$r)
+          ncol <- ncol(cor$r)
+	  ncex <- 1.8-0.055*max(ncol, nrow)
+          #self$results$pre$setContent(nrow)
+	  p <- corrplot::corrplot(cor$r, p.mat=cor$p,
+		tl.cex=1.5, tl.col="black",
+		mar=c(0, 0, ifelse(cor$m>"",1,0), 0), main=cor$m,
+		col=col, number.cex=ncex,
+		cl.pos="n",	#ifelse(nrow<3,"n",self$options$clPos),
+		cl.cex=1.1, cl.ratio=0,
+		sig.level=sigl,
+		insig=ifelse(ULe, "blank", insig), pch="",
+		method=pm,
+                addshade=c("negative", "positive", "all")[3], shade.lwd=3,
+		type="full")
+
+	  if (self$options$clPos=="r") {
+            vert = TRUE
+            xlim = c(ncol+0.5, ncol+1.1)
+            ylim = c(0.5, nrow+0.5)
+	  } else {
+            vert = FALSE
+            xlim = c(0.5, ncol+0.5)
+            ylim = c(0, 0.6)
+          }
+#	  if (self$options$clPos!="n" && nrow<3)
+#	    corrplot::colorlegend(col, labels=c(seq(-1,1,.2)), align="c",
+#		cex=1.1, vertical=vert, addlabels=TRUE,
+#		xlim=xlim, ylim=ylim, ratio.colbar=0.15)
+	  print(p)
+	  return(TRUE)
+      },
+
+      .treeplot=function(image, ggtheme, theme, ...) {
+          nVars  <- length(self$options$vars)
+          if (is.null(image$state) || nVars<3) return(FALSE)
+          nClust <- ifelse(self$options$numClust>nVars,nVars,self$options$numClust)
+          hc <- image$state$hc
+          main <- image$state$s
+          if (self$options$clustCol) {
+            hd <- as.dendrogram(hc)
+            cols <- jmvcore::colorPalette(n=nVars, theme$palette, type="fill")
+            hd <- dendextend::color_branches(hd, k=nClust)#, col=cols)
+            hd <- dendextend::color_labels(hd, k=nClust)	#, col=cols)
+            hd <- dendextend::set(hd, "branches_lwd", 3)
+            hd <- dendextend::set(hd, "labels_cex", 1.5)
+            if (main>"") opar <- par(mar=c(10,0,1,0))
+            else opar <- par(mar=c(10,0,0,0))
+            p <- plot(hd, axes=FALSE, main=main)
+          } else {
+            if (main>"") opar <- par(mar=c(0,0,1,0))
+            else opar <- par(mar=c(0,0,0,0))
+            p <- plot(hc, col=1:10, axes=FALSE, ann=TRUE, cex=1.5,
+                 main=main, lwd=2, hang=-1)
+          }
+
+          cutree_k_to_h <- function(tree, k) {
+            n1 <- nrow(tree$merge)
+            n  <- n1 + 1
+            mean(tree$height[c(n-k, n-k+1L)])
+          }
+          abline(h=cutree_k_to_h(hc, nClust), col="red", lwd=2, lty=2)
+
+	  print(p)
+	  return(TRUE)
+      }
+))
---
+++ R/mroc.b.R
@@ -0,0 +1,204 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+mROCClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "mROCClass",
+    inherit = mROCBase,
+    private = list(
+      .init=function() {
+	  if (grepl("Russian", Sys.getlocale(), fixed=TRUE)) options(OutDec=",")
+      },
+
+      .run = function() {
+	  vars  <- self$options$vars
+	  nVars <- length(vars)
+          dat   <- data.frame(self$data, check.names=FALSE)
+          for (var in vars) {
+            image <- self$results$plots$get(key=var)
+            image$setState(list(data=dat))
+            image <- self$results$splots$get(key=var)
+            image$setState(list(data=dat))
+          }
+          image <- self$results$plot
+          image$setState(list(data=dat))
+	  #if (self$options$sepROC) image$visible=FALSE
+          # `self$data` contains the data
+          # `self$options` contains the options
+          # `self$results` contains the results object (to populate)
+      },
+
+      .plot=function(image, ggtheme, theme, ...) {
+	  vars  <- self$options$vars
+	  nVars <- length(vars)
+          key   <- image$key
+          split <- self$options$splitROC && !is.na(key)
+	  dat   <- image$state$data
+
+	  levels<- c("")
+	  if (!is.na(key)) {
+	    if (self$options$sepROC) {
+	      grp    <- self$options$groups
+              grps   <- jmvcore::select(dat, grp)
+              levels <- base::levels(grps[!is.na(grps[[grp]]), grp])
+	      vars <- levels
+	    } else {
+	      vars   <- c(key)
+	    }
+	  }
+
+	  # data subset
+	  subs <- ""
+          self$results$text$setContent(" ")
+	  #if (!is.null(self$options$groups) && (!self$options$sepROC || is.na(key))) {
+	  if (!is.null(self$options$groups) && !self$options$sepROC) {
+	    subs <- paste(self$options$groups, "==\"", self$options$selgroup,"\"", sep="")
+            #self$results$text$setContent(paste("<h2>", subs, "</h2>", sep=""))
+	    sub <- paste("`", self$options$groups, "`==\"", self$options$selgroup,"\"", sep="")
+            dat <- subset(dat, eval(parse(text=sub)))
+          }
+          cls   <- jmvcore::select(dat, self$options$class)
+          lev   <- base::levels(cls[!is.na(cls[[self$options$class]]), self$options$class])
+
+	  d   <- 0
+	  add <- FALSE
+	  AUC <- NULL
+	  DIR <- NULL
+	  TR  <- NULL
+	  TC  <- NULL
+	  oROC<- NULL
+	  dL  <- self$options$cmpDeLong
+
+	  cols <- jmvcore::colorPalette(n=nVars, theme$palette, type="fill")
+	  if (self$options$palBrewer!="none")
+		#https://earlglynn.github.io/RNotes/package/RColorBrewer/index.html
+		cols <- RColorBrewer::brewer.pal(n=nVars, name=self$options$palBrewer)
+	  if (self$options$BW && self$options$dotline) cols <- rep(1, nVars)
+	  nVars <- length(vars)
+
+	  for (var in vars) {
+	    d <- d+1
+
+	    if (split) d <- which(sapply(self$options$vars, function(X) key %in% X))[[1]]
+
+	    if (is.na(key) || split) {
+	      kk <- ""
+	      dd <- dat
+	      pair <- TRUE
+              frm <- stats::as.formula(jmvcore:::composeFormula(self$options$class, var))
+	    } else if (!is.na(key)) {
+	      kk  <- self$options$groups
+	      sub <- paste("`", self$options$groups, "`==\"", var,"\"", sep="")
+	      dd <- subset(dat, eval(parse(text=sub)))
+	      pair <- FALSE
+              frm <- stats::as.formula(jmvcore:::composeFormula(self$options$class, image$key))
+	    }
+
+	    #self$results$pre$setContent(frm)
+
+	    roc <- pROC::roc(frm, data=dd, percent=self$options$perc)
+	    rci <- pROC::ci(roc, conf.level=self$options$ciWidth/100)
+
+	    AUC <- rbind(AUC, c(rci))
+	    DIR <- rbind(DIR, ifelse(self$options$direction,
+		paste(", ", substr(lev[1],1,1), roc$direction, substr(lev[2],1,1), sep=""),""))
+
+            if (dL!="none") {
+              if (add) {
+                rt <- pROC::roc.test(roc, oROC, paired=pair,
+			parallel=TRUE, method="d", alternative="two.sided")
+		pv <- jmvcore:::formatElement(rt$p.value, p=TRUE, zto=FALSE, sf=3, dp=3)
+		pv <- gsub(" ", "0", pv)
+		if (self$options$mAst)
+		  TR <- rbind(TR, paste(" ",private$.asterisk(rt$p.value)))
+		else
+		  TR <- rbind(TR, paste(", p", ifelse(rt$p.value>0.001,"=",""),
+			pv, sep=""))
+                #TR <- rbind(TR, paste(", p=", signif(rt$p.value, 2), sep=""))
+                if (rt$p.value<0.05 && !self$options$BW) TC <- rbind(TC, "red")
+                else TC <- rbind(TC, "black")
+              } else {
+                TR <- rbind(TR, "")
+                TC <- rbind(TC, "black")
+              }
+	      if (dL!="wcontr" || is.null(oROC)) oROC <- roc
+            } else TR <- ""
+	    fmt  <- ifelse(self$options$perc,'%#.1f%%','%#.3f')
+
+	    fauc <- paste("AUC ", fmt)
+	    if (self$options$ciAUC)
+		fauc <- paste(fauc, " [",  sprintf(gsub("%%","",fmt), AUC[,1]),
+		"-",  sprintf(gsub("%%","",fmt), AUC[,3]), "]", sep="")
+	    if (self$options$perc) fauc <- paste(gsub("%%","",fauc),"%%",sep="")
+
+	    main <- ifelse(nVars<2, var, "")
+	    main <- ifelse(self$options$direction,
+		ifelse(nVars<2, paste(main, " (", lev[1], roc$direction, lev[2], ")", sep=""),
+		paste(lev, collapse=' / ')), main)
+	    main <- ifelse(subs>"", paste(main, " (", subs, ")", sep=""), main)
+	    main <- ifelse(is.na(key)||split, main, paste(image$key, " {", main, "}", sep=""))
+
+	    bss  <- ifelse(self$options$theBest %in% c("bestY","bssY"), "youden", "closest.topleft")
+	    best <- pROC::coords(roc, "best", best.method=bss, transpose=TRUE)
+
+	    p <- pROC::plot.roc(frm, data=dd, col=cols[d],
+		conf.level=self$options$ciWidth/100,
+		main=main, cex.main=1.3,
+		percent=self$options$perc,
+		lty=ifelse(self$options$dotline, d, 1),
+		cex.lab=1.5, cex.axis=1.3, lwd=self$options$lwd,
+		auc.polygon=self$options$polygon, auc.polygon.col=cols[d], auc.polygon.density=15,
+		print.auc=nVars<2, print.auc.cex=1.5,
+		print.auc.pattern=fauc,
+		print.thres=ifelse(self$options$theBest=="none", FALSE, TRUE),
+		print.thres.col=cols[d],
+		print.thres.pch=19, print.thres.cex=1.3,
+		print.thres.pattern=ifelse(self$options$theBest %in% c("bssC","bssY"), paste("%.2f"," (",fmt,", ",fmt,")", sep=""), "%.2f"),
+		print.thres.best.method=bss,
+		legacy.axes=self$options$legacy,
+		xlab=ifelse(roc$percent, ifelse(self$options$legacy, .("100 - Specificity (%)"), .("Specificity (%)")), ifelse(self$options$legacy, .("1 - Specificity"), .("Specificity"))),
+		ylab=ifelse(roc$percent, .("Sensitivity (%)"), .("Sensitivity")),
+		ci=(self$options$ciAUC && self$options$ciMark && self$options$theBest!="none"),
+		ci.col=cols[d],
+		of="thresholds", thresholds=best, #thresholds="best",
+		#ci.type=c("bars", "shape", "no")[2],
+		#ci.col=ifelse(ci.type=="bars", par("fg"), "gainsboro"),
+		grid=!add, add=add
+		)
+	    if (!add) add <- TRUE
+	  }
+	  if (self$options$ciAUC && split) {
+		#ciobj <- pROC::ci.se(p, specificities=seq(0, 100, 5))
+		#plot(ciobj, type="shape", col="#1c61b6AA")
+		#plot(pROC::ci(p, of="thresholds", thresholds = "best")) # add one threshold
+	  }
+	  if (nVars>1) {
+	      fmt <- ifelse(self$options$perc,'%#.1f','%#.3f')
+	      au <- sprintf(AUC[,2], fmt=fmt)
+	      ci <- paste(sprintf(AUC[,1], fmt=fmt), "-", sprintf(AUC[,3], fmt=fmt), sep="")
+	      if (self$options$ciAUC) au <- paste(au, " [", ci, "]", sep="")
+	      tit <- paste(kk," (AUC", 
+		ifelse(self$options$ciAUC, jmvcore::format(.(" [{prc}% CI]"), prc=self$options$ciWidth),""),
+		ifelse(self$options$perc, "%",""), ")", sep="")
+	      if (self$options$perc) au <- paste(au, "%", sep="")
+
+	      legend(title=tit, "bottomright",
+		cex=1.2, lwd=self$options$lwd, col=cols,
+		bg=ifelse(self$options$polygon || self$options$legBox, "white", "transparent"),
+		box.lwd=ifelse(self$options$polygon || self$options$legBox, 1, 0),
+		lty=1:ifelse(self$options$dotline, length(self$options$vars), 1),
+		text.col=TC,
+		legend=paste(vars, " (", au, ")", DIR, TR, sep="")
+		)
+	  }
+
+	  print(p)
+	  return(TRUE)
+      },
+
+      .asterisk=function(pval) {
+	if (pval < .001)     return("***")
+	else if (pval < .01) return("**")
+	else if (pval < .05) return("*")
+      }
+    )
+)
